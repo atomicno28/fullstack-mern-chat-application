c0cc064851901769a220d4f38509a7ad99d13ff1
Nikhil Patel <nikhilid2205@gmail.com>
2023-06-15 09:43:26 +0530

    Sidebar

---
+++ src/layouts/dashboard/index.js
@@ -0,0 +1,184 @@
+import React from "react";
+import { useState } from "react";
+import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { Outlet } from "react-router-dom";
+
+import Logo from "../../assets/Images/logo.ico";
+
+import { Nav_Buttons } from "../../data";
+import { Gear } from "phosphor-react";
+
+import { faker } from "@faker-js/faker";
+import styled from "@emotion/styled";
+
+import useSettings from "../../hooks/useSettings";
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(20px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
+
+const DashboardLayout = () => {
+  const theme = useTheme();
+
+  const [selected, setSelected] = useState(0);
+
+  // use setting is custom hook.
+  const { onToggleMode } = useSettings();
+
+  console.log(theme);
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.25)",
+          height: "100vh",
+          width: 100,
+        }}
+      >
+        {/* wrapping everything inside the column. */}
+        <Stack
+          direction="column"
+          alignItems={"center"}
+          justifyContent="space-between"
+          sx={{ height: "100%" }}
+          spacing={3}
+        >
+          <Stack spacing={4} alignItems={"center"}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 2.5,
+              }}
+            >
+              <img src={Logo} alt="ChatAPP" />
+            </Box>
+
+            {/* buttons */}
+            <Stack
+              sx={{ width: "max-content" }}
+              direction="column"
+              alignItems="center"
+              spacing={3}
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => setSelected(el.index)}
+                    sx={{
+                      width: "max-content",
+                      color:
+                        theme.palette.mode === "light"
+                          ? "#000"
+                          : theme.palette.text.primary,
+                    }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected === 3 ? (
+                <Box
+                  p={1}
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  sx={{
+                    width: "max-content",
+                    color:
+                      theme.palette.mode === "light"
+                        ? "#000"
+                        : theme.palette.text.primary,
+                  }}
+                  onClick={() => setSelected(3)}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+          <Stack spacing={4}>
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
+            <Avatar src={faker.image.avatar()} />
+          </Stack>
+        </Stack>
+      </Box>
+      <Outlet />
+    </>
+  );
+};
+
+export default DashboardLayout;
