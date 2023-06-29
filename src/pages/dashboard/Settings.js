904c0fdef4f6eceeff12b128d8600d44fd19c9b4
Nikhil Patel <nikhilid2205@gmail.com>
2023-06-29 15:56:01 +0530

    Routes

---
+++ src/pages/dashboard/Settings.js
@@ -0,0 +1,17 @@
+import { Box, Stack } from "@mui/material";
+import React from "react";
+
+const Settings = () => {
+  return (
+    <>
+      <Stack direction={"row"} sc={{ width: "100%" }}>
+        {/* Left Panel */}
+        <div className="scrollbar" overflowY=""></div>
+        <Box sx={{}}></Box>
+        {/* Right Panel */}
+      </Stack>
+    </>
+  );
+};
+
+export default Settings;
