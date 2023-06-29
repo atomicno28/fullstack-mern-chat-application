de22762730a1fbbcb4fd8b895e14a96aaa48c976
Nikhil Patel <nikhilid2205@gmail.com>
2023-06-29 15:55:36 +0530

    REDUX dispatch

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -3,24 +3,46 @@ import React from "react";
 import Chats from "./Chats";
 import { Box, Stack, useTheme } from "@mui/material";
 import Conversation from "../../components/Conversation";
+import Contact from "../../components/Contact";
+import { useSelector } from "react-redux";
+import SharedMessages from "../../components/SharedMessages";
+import StarredMessages from "../../components/StarredMessages";
 
 const GeneralApp = () => {
   const theme = useTheme();
+  const { sidebar } = useSelector((store) => store.app);
+
   return (
     <Stack direction="rows" sx={{ width: "100%" }}>
       <Chats />
       <Box
         sx={{
           height: "100%",
-          width: "calc(100vw - 420px)",
+          width: sidebar.open ? "calc(100vw - 740px)" : "calc(100vw - 420px)",
           backgroundColor:
             theme.palette.mode === "light"
               ? "#F0F4FA"
               : theme.palette.background.default,
         }}
       >
+        {/* Conversation */}
         <Conversation />
       </Box>
+      {/* Contact  */}
+      {/* {sidebar.open && <Contact />} */}
+      {sidebar.open &&
+        (() => {
+          switch (sidebar.type) {
+            case "CONTACT":
+              return <Contact />;
+            case "STARRED":
+              return <StarredMessages />;
+            case "SHARED":
+              return <SharedMessages />;
+            default:
+              break;
+          }
+        })()}
     </Stack>
   );
 };
